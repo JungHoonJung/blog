ce9af64c43a88eea9b2ab8497813ad85410448c4
junghoonjung <tydhjjh9311@gmail.com>
2019-12-09 03:29:44 +0900

    12.08

--- source/conf.py
+++ source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Junghoon Jung'
 author = 'Junghoon Jung'
 
 # The short X.Y version
-version = '2019.12.04'
+version = '2019.12.08'
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '12.08'
 
 
 # -- General configuration ---------------------------------------------------
